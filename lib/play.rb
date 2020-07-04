dd17242d68a9c6462d06d59eedc1674f0fa8e122
Deja Monae Allison <dejaallison@gmail.com>
2020-07-04 00:39:45 +0000

    Automatically backed up by Learn

--- lib/play.rb
+++ lib/play.rb
@@ -36,12 +36,11 @@ def turn(board)
 end
 
 def play(board = "X")
-    board[index] = board
-  count = 0
-  board.each do |index|
-   if index != " "
-     count += 1
-   end
+  counter = 0
+   board.each {|space|
+     if space == "X" || space == "O"
+       counter += 1
+     end
+   }
+   counter
  end
-   count
-end
